695c906e3388c9a27a3f9bba15aa3bf7d813cc9a
Bernardo Martínez Garrido <programming@wandrell.com>
2014-07-08 23:57:05 +0200

    Corrected the name of the additional belongings DAO.

--- src/main/java/com/wandrell/tabletop/pendragon/framework/persistence/AdditionalBelongingsDAO.java
+++ src/main/java/com/wandrell/tabletop/pendragon/framework/persistence/AdditionalBelongingsDAO.java
@@ -2,10 +2,11 @@ package com.wandrell.tabletop.pendragon.framework.persistence;
 
 import java.util.Collection;
 
+import com.wandrell.tabletop.dice.RollTable;
 import com.wandrell.tabletop.pendragon.inventory.AdditionalBelongings;
 
-public interface InitialEquipmentDAO {
+public interface AdditionalBelongingsDAO {
 
-    public Collection<AdditionalBelongings> getInitialEquipment();
+    public Collection<RollTable<AdditionalBelongings>> getAdditionalBelongings();
 
 }
