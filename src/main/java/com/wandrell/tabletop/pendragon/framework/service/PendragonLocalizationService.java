023fc361ea1885ccd746c4617838176999cd6e6d
Bernardo Martínez Garrido <programming@wandrell.com>
2014-07-12 13:58:01 +0200

    Changed the localization service methods.

--- src/main/java/com/wandrell/tabletop/pendragon/framework/service/PendragonLocalizationService.java
+++ src/main/java/com/wandrell/tabletop/pendragon/framework/service/PendragonLocalizationService.java
@@ -2,9 +2,21 @@ package com.wandrell.tabletop.pendragon.framework.service;
 
 public interface PendragonLocalizationService {
 
+    public String getAdditionalBelongingsString(final String key);
+
+    public String getButtonString(final String key);
+
     public String getDerivedAttributeString(final String key);
 
+    public String getFamilyCharacteristicString(final String key);
+
+    public String getLabelString(final String key);
+
+    public String getRollTableString(String key);
+
     public String getSkillString(final String key);
+
+    public String getTitleString(final String key);
 
     public String getTraitString(final String key);
 
