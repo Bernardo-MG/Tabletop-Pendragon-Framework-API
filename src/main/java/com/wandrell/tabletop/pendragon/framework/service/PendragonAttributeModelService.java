64b308985b9a995f023bd75d52786c34d407b2b1
Bernardo Martínez Garrido <programming@wandrell.com>
2014-07-16 18:12:01 +0200

    Added a service for the attributes.

---
+++ src/main/java/com/wandrell/tabletop/pendragon/framework/service/PendragonAttributeModelService.java
@@ -0,0 +1,11 @@
+package com.wandrell.tabletop.pendragon.framework.service;
+
+import java.util.Collection;
+
+import com.wandrell.tabletop.pendragon.valuehandler.PendragonAttribute;
+
+public interface PendragonAttributeModelService {
+
+    public Collection<PendragonAttribute> getAttributes();
+
+}
