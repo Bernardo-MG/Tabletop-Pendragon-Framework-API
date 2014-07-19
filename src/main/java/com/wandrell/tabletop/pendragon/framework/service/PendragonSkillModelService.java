e3282c3b14c9b1542494999720382519d25f15e9
Bernardo Martínez Garrido <programming@wandrell.com>
2014-07-19 21:11:23 +0200

    Added a service for Pendragon skills.

---
+++ src/main/java/com/wandrell/tabletop/pendragon/framework/service/PendragonSkillModelService.java
@@ -0,0 +1,11 @@
+package com.wandrell.tabletop.pendragon.framework.service;
+
+import java.util.Collection;
+
+import com.wandrell.tabletop.pendragon.valuehandler.PendragonSkill;
+
+public interface PendragonSkillModelService {
+
+    public Collection<PendragonSkill> getBaseSkills();
+
+}
