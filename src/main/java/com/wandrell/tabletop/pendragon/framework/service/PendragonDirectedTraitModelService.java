a77d0f2e41e65bad0f82191a052c1296ac2da2b5
Bernardo Martínez Garrido <programming@wandrell.com>
2014-07-27 20:06:24 +0200

    Added a service for Directed Traits.

---
+++ src/main/java/com/wandrell/tabletop/pendragon/framework/service/PendragonDirectedTraitModelService.java
@@ -0,0 +1,11 @@
+package com.wandrell.tabletop.pendragon.framework.service;
+
+import java.util.Collection;
+
+import com.wandrell.tabletop.pendragon.valuehandler.PendragonDirectedTrait;
+
+public interface PendragonDirectedTraitModelService {
+
+    public Collection<PendragonDirectedTrait> getDirectedTraits();
+
+}
