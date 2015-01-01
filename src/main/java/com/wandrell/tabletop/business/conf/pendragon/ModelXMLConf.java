2642cd3941d98a2014e52ac2181979ed1dc9fd31
Bernardo Martínez Garrido <programming@wandrell.com>
2015-01-01 22:51:08 +0100

    Extended the model service for family characteristics.

--- src/main/java/com/wandrell/tabletop/business/conf/pendragon/ModelXMLConf.java
+++ src/main/java/com/wandrell/tabletop/business/conf/pendragon/ModelXMLConf.java
@@ -2,19 +2,23 @@ package com.wandrell.tabletop.business.conf.pendragon;
 
 public final class ModelXMLConf {
 
-    public static final String ARMOR_BONUS        = "armor_bonus";
-    public static final String BONUS              = "bonus";
-    public static final String DAMAGE_BONUS       = "damage_bonus";
-    public static final String DAMAGE_DICE_BONUS  = "damage_dice_bonus";
-    public static final String DERIVED_ATTRIBUTES = "derived_attributes";
-    public static final String DESCRIPTOR         = "descriptor";
-    public static final String DIRECTED_TRAITS    = "directed_traits";
-    public static final String NAME               = "name";
-    public static final String PASSIONS           = "passions";
-    public static final String SKILLS             = "skills";
-    public static final String SPECIALTY_SKILLS   = "specialty_skills";
-    public static final String TRAITS             = "traits";
-    public static final String VALUE              = "value";
+    public static final String ARMOR_BONUS           = "armor_bonus";
+    public static final String ATTRIBUTES            = "attributes";
+    public static final String BONUS                 = "bonus";
+    public static final String DAMAGE_BONUS          = "damage_bonus";
+    public static final String DAMAGE_DICE_BONUS     = "damage_dice_bonus";
+    public static final String DERIVED_ATTRIBUTES    = "derived_attributes";
+    public static final String DESCRIPTOR            = "descriptor";
+    public static final String DIRECTED_TRAITS       = "directed_traits";
+    public static final String FAMILY_CHARACTERISTIC = "family_characteristic";
+    public static final String INTERVALS             = "intervals";
+    public static final String LOWER_LIMIT           = "lower_limit";
+    public static final String NAME                  = "name";
+    public static final String PASSIONS              = "passions";
+    public static final String SKILLS                = "skills";
+    public static final String SPECIALTY_SKILLS      = "specialty_skills";
+    public static final String TRAITS                = "traits";
+    public static final String VALUE                 = "value";
 
     private ModelXMLConf() {
         super();
--- src/main/java/com/wandrell/tabletop/business/service/pendragon/ModelService.java
+++ src/main/java/com/wandrell/tabletop/business/service/pendragon/ModelService.java
@@ -3,11 +3,22 @@ package com.wandrell.tabletop.business.service.pendragon;
 import java.util.Collection;
 import java.util.Map;
 
+import com.wandrell.tabletop.business.model.interval.Interval;
+import com.wandrell.tabletop.business.model.pendragon.chargen.FamilyCharacteristicTable;
+import com.wandrell.tabletop.business.model.pendragon.chargen.FamilyCharacteristicTemplate;
 import com.wandrell.tabletop.business.model.pendragon.chargen.HomelandTemplate;
 import com.wandrell.tabletop.business.model.pendragon.chargen.ReligionTemplate;
 import com.wandrell.tabletop.business.model.skill.NameAndDescriptor;
 
 public interface ModelService {
+
+    public FamilyCharacteristicTable getFamilyCharacteristicTable(
+            final String name,
+            final Map<Interval, FamilyCharacteristicTemplate> intervals);
+
+    public FamilyCharacteristicTemplate getFamilyCharacteristicTemplate(
+            final String name, final Map<String, Integer> attributes,
+            final Map<NameAndDescriptor, Integer> skills);
 
     public HomelandTemplate getHomelandTemplate(final String name,
             final Map<NameAndDescriptor, Integer> skills,
@@ -20,8 +31,5 @@ public interface ModelService {
             final Collection<String> traits,
             final Map<String, Integer> bonusDerived, final Integer bonusArmor,
             final Integer bonusDamage, final Integer bonusDamageDice);
-
-    public NameAndDescriptor getSkillData(final String name,
-            final String descriptor);
 
 }
