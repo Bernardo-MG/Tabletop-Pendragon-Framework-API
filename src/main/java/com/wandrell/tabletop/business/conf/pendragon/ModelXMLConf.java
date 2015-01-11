d2e347d267d6277bb2d4b8d0ded7475ca829964f
Bernardo Martínez Garrido <programming@wandrell.com>
2015-01-11 17:18:56 +0100

    Added method to acquire inventory classes.

--- src/main/java/com/wandrell/tabletop/business/conf/pendragon/ModelXMLConf.java
+++ src/main/java/com/wandrell/tabletop/business/conf/pendragon/ModelXMLConf.java
@@ -3,18 +3,30 @@ package com.wandrell.tabletop.business.conf.pendragon;
 public final class ModelXMLConf {
 
     public static final String ARMOR_BONUS                = "armor_bonus";
+    public static final String ARMOR_TYPE                 = "armor_type";
+    public static final String ARMOR_VALUE                = "armor_value";
     public static final String ATTRIBUTES                 = "attributes";
     public static final String BASE                       = "base";
     public static final String BASE_GLORY                 = "base_glory";
     public static final String BONUS                      = "bonus";
+    public static final String BREAK_ENEMY_ON_DRAW        = "break_enemy_on_draw";
+    public static final String BREAK_ON_FUMBLE            = "break_on_fumble";
     public static final String COMBAT                     = "combat";
     public static final String COURTLY                    = "courtly";
     public static final String DAMAGE_BONUS               = "damage_bonus";
     public static final String DAMAGE_DICE_BONUS          = "damage_dice_bonus";
+    public static final String DAMAGE_OVERRIDE            = "damage_override";
     public static final String DERIVED_ATTRIBUTES         = "derived_attributes";
+    public static final String DESCRIPTION                = "description";
     public static final String DESCRIPTOR                 = "descriptor";
+    public static final String DEXTERITY_MODIFIER         = "dexterity_modifier";
+    public static final String DICE_BONUS                 = "dice_bonus";
     public static final String DIRECTED_TRAITS            = "directed_traits";
     public static final String FAMILY_CHARACTERISTIC      = "family_characteristic";
+    public static final String FLAGS                      = "flags";
+    public static final String HEAVY                      = "heavy";
+    public static final String HITS_BACK                  = "hits_back";
+    public static final String IGNORES_SHIELD             = "ignores_shield";
     public static final String INTERVALS                  = "intervals";
     public static final String KNIGHTLY                   = "knightly";
     public static final String KNOWLEDGE                  = "knowledge";
@@ -22,7 +34,11 @@ public final class ModelXMLConf {
     public static final String MONEY                      = "money";
     public static final String NAME                       = "name";
     public static final String PASSIONS                   = "passions";
+    public static final String RANGE                      = "range";
+    public static final String RATE_OF_FIRE               = "rate_of_fire";
+    public static final String REDUCE_SHIELD_TO_ROLL      = "reduce_shield_to_roll";
     public static final String REPEATEABLE                = "repeatable";
+    public static final String SKILL                      = "skill";
     public static final String SKILLS                     = "skills";
     public static final String SKILLS_GROUP               = "skills_group";
     public static final String SKILLS_GROUP_POINTS_BONUS  = "skills_points_group_bonus";
@@ -31,8 +47,11 @@ public final class ModelXMLConf {
     public static final String SKILLS_POINTS              = "skills_points";
     public static final String SPECIALTY_SKILLS           = "specialty_skills";
     public static final String TRAITS                     = "traits";
+    public static final String TWO_HANDED                 = "two_handed";
+    public static final String TYPE                       = "type";
     public static final String VALUE                      = "value";
     public static final String VALUES                     = "values";
+    public static final String VS_ARMOR_BONUS             = "vs_armor_bonus";
     public static final String YEARLY_GLORY               = "yearly_glory";
 
     private ModelXMLConf() {
--- src/main/java/com/wandrell/tabletop/business/service/pendragon/ModelService.java
+++ src/main/java/com/wandrell/tabletop/business/service/pendragon/ModelService.java
@@ -11,6 +11,12 @@ import com.wandrell.tabletop.business.model.pendragon.chargen.FatherClassTemplat
 import com.wandrell.tabletop.business.model.pendragon.chargen.HomelandTemplate;
 import com.wandrell.tabletop.business.model.pendragon.chargen.ReligionTemplate;
 import com.wandrell.tabletop.business.model.pendragon.glory.FatherClassGlory;
+import com.wandrell.tabletop.business.model.pendragon.inventory.Armor;
+import com.wandrell.tabletop.business.model.pendragon.inventory.ArmorType;
+import com.wandrell.tabletop.business.model.pendragon.inventory.Item;
+import com.wandrell.tabletop.business.model.pendragon.inventory.Money;
+import com.wandrell.tabletop.business.model.pendragon.inventory.Shield;
+import com.wandrell.tabletop.business.model.pendragon.inventory.Weapon;
 import com.wandrell.tabletop.business.model.pendragon.stats.DirectedTrait;
 import com.wandrell.tabletop.business.model.pendragon.stats.Passion;
 import com.wandrell.tabletop.business.model.pendragon.stats.Skill;
@@ -19,6 +25,10 @@ import com.wandrell.tabletop.business.model.pendragon.util.TextList;
 import com.wandrell.tabletop.business.model.skill.NameAndDescriptor;
 
 public interface ModelService {
+
+    public Armor getArmor(final String name, final String description,
+            final Money money, final ArmorType type, final Integer armorValue,
+            final Integer dexModifier, final Boolean heavyLoad);
 
     public DirectedTrait getDirectedTrait(final String name);
 
@@ -49,6 +59,10 @@ public interface ModelService {
             final Collection<NameAndDescriptor> directedTraits,
             final Collection<NameAndDescriptor> passions);
 
+    public Item getItem(final String name, final String description);
+
+    public Money getMoney(final Integer denarii, final Integer libra);
+
     public Passion getPassion(final String name, final String descriptor,
             final Boolean repeatable);
 
@@ -56,6 +70,9 @@ public interface ModelService {
             final Collection<String> traits,
             final Map<String, Integer> bonusDerived, final Integer bonusArmor,
             final Integer bonusDamage, final Integer bonusDamageDice);
+
+    public Shield getShield(final String name, final String description,
+            final Money money, final Integer armorValue);
 
     public Skill getSkill(final String name, final String descriptor,
             final Boolean combat, final Boolean court, final Boolean knight,
@@ -66,5 +83,14 @@ public interface ModelService {
 
     public TextList getTextList(final String name,
             final Collection<String> values);
+
+    public Weapon getWeapon(final String name, final String description,
+            final Money money, final String skill, final Boolean twoHanded,
+            final Integer damageBonus, final Integer diceBonus,
+            final Integer damageOverride, final Integer maxRange,
+            final Integer rof, final Map<ArmorType, Integer> armorBonus,
+            final Boolean breaksEnemyOnDraw, final Boolean breaksOnFumble,
+            final Boolean hitsBack, final Boolean ignoresShield,
+            final Boolean reducesShieldToRoll);
 
 }
