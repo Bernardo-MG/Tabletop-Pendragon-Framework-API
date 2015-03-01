e91c06b52adf2a00563776ed41447624accb727c
Bernardo Martínez Garrido <programming@wandrell.com>
2015-03-01 15:21:33 +0100

    All the methods referring to skills say so on their names.

--- src/main/java/com/wandrell/tabletop/pendragon/service/chargen/CharGenRulesetService.java
+++ src/main/java/com/wandrell/tabletop/pendragon/service/chargen/CharGenRulesetService.java
@@ -5,7 +5,7 @@ import com.wandrell.tabletop.pendragon.model.stats.Skill;
 
 public interface CharGenRulesetService {
 
-    public Boolean canAddIndividualDifference(final Skill skill,
+    public Boolean canAddIndividualDifferenceSkill(final Skill skill,
             final Gender gender);
 
     public Boolean canBecomeExcellentSkill(final Skill skill,
