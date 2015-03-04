c6a9d310d8f5287b55867cb3545339b9020282ca
Bernardo Martínez Garrido <programming@wandrell.com>
2015-03-04 17:49:24 +0100

    Renamed methods for clarity.

--- src/main/java/com/wandrell/tabletop/pendragon/service/chargen/IndividualDifferencesRulesetService.java
+++ src/main/java/com/wandrell/tabletop/pendragon/service/chargen/IndividualDifferencesRulesetService.java
@@ -4,12 +4,6 @@ import com.wandrell.tabletop.pendragon.model.stats.Skill;
 
 public interface IndividualDifferencesRulesetService {
 
-    public Boolean canBecomeExcellentSkill(final Skill skill);
-
-    public Boolean canBecomeExtraSkill(final Skill skill);
-
-    public Boolean canGetSkillRaise(final Skill skill);
-
     public Integer getExcellentSkillValue();
 
     public Integer getExtraSkillMaxPicks();
@@ -19,5 +13,11 @@ public interface IndividualDifferencesRulesetService {
     public Integer getNoCombatSkillsPoints();
 
     public Integer getSkillRaiseValue();
+
+    public Boolean isAbleToBecomeExcellentSkill(final Skill skill);
+
+    public Boolean isAbleToBecomeExtraSkill(final Skill skill);
+
+    public Boolean isAbleToGetSkillRaise(final Skill skill);
 
 }
