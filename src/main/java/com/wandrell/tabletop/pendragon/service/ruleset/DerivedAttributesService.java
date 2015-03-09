3d1830a56c95734bd3ab55cbbc46297cafd3d469
Bernardo Martínez Garrido <programming@wandrell.com>
2015-03-09 22:31:45 +0100

    The Derived Attributes service now asks just for the character.

--- src/main/java/com/wandrell/tabletop/pendragon/service/ruleset/DerivedAttributesService.java
+++ src/main/java/com/wandrell/tabletop/pendragon/service/ruleset/DerivedAttributesService.java
@@ -1,41 +1,27 @@
 package com.wandrell.tabletop.pendragon.service.ruleset;
 
+import com.wandrell.tabletop.pendragon.model.character.PendragonBaseCharacter;
+import com.wandrell.tabletop.pendragon.model.character.PendragonHumanCharacter;
+
 public interface DerivedAttributesService {
 
-    public Integer getDamage(final Integer appearance,
-            final Integer constitution, final Integer dexterity,
-            final Integer size, final Integer strength);
+    public Integer getDamage(final PendragonBaseCharacter character);
 
-    public Integer getDistinctiveFeatures(final Integer appearance,
-            final Integer constitution, final Integer dexterity,
-            final Integer size, final Integer strength);
+    public Integer getDistinctiveFeatures(
+            final PendragonHumanCharacter character);
 
-    public Integer getHealingRate(final Integer appearance,
-            final Integer constitution, final Integer dexterity,
-            final Integer size, final Integer strength);
+    public Integer getHealingRate(final PendragonBaseCharacter character);
 
-    public Integer getHitPoints(final Integer appearance,
-            final Integer constitution, final Integer dexterity,
-            final Integer size, final Integer strength);
+    public Integer getHitPoints(final PendragonBaseCharacter character);
 
-    public Integer getKnockdown(final Integer appearance,
-            final Integer constitution, final Integer dexterity,
-            final Integer size, final Integer strength);
+    public Integer getKnockdown(final PendragonBaseCharacter character);
 
-    public Integer getMajorWound(final Integer appearance,
-            final Integer constitution, final Integer dexterity,
-            final Integer size, final Integer strength);
+    public Integer getMajorWound(final PendragonBaseCharacter character);
 
-    public Integer getMoveRate(final Integer appearance,
-            final Integer constitution, final Integer dexterity,
-            final Integer size, final Integer strength);
+    public Integer getMoveRate(final PendragonBaseCharacter character);
 
-    public Integer getUnconcious(final Integer appearance,
-            final Integer constitution, final Integer dexterity,
-            final Integer size, final Integer strength);
+    public Integer getUnconcious(final PendragonBaseCharacter character);
 
-    public Integer getWeight(final Integer appearance,
-            final Integer constitution, final Integer dexterity,
-            final Integer size, final Integer strength);
+    public Integer getWeight(final PendragonBaseCharacter character);
 
 }
